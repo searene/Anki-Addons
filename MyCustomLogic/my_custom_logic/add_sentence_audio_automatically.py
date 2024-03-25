676c669a136c574b7f299f967547d6800813eb96
Joey <mycityofsky@gmail.com>
2024-03-25 21:42:49 +0800

    return the correct value in fill_sentence_voice_automatically

--- MyCustomLogic/my_custom_logic/add_sentence_audio_automatically.py
+++ MyCustomLogic/my_custom_logic/add_sentence_audio_automatically.py
@@ -20,36 +20,35 @@ def get_field_by_name(target_field_name: str, note: Note) -> Optional[FieldDict]
     return None
 
 
-def fill_sentence_voice_automatically(changed: bool, note: Note, field_idx: int):
+def fill_sentence_voice_automatically(changed: bool, note: Note, field_idx: int) -> bool:
     """Fetch the audio file from Azure TTS service and fill the Sentence Voice field automatically."""
     current_note_name = note.note_type()['name']
     if current_note_name not in ("English - Complete", "English - Complete - With Reverse"):
-        return
+        # If nothing is changed, we just return "changed".
+        # The official Anki website illustrates the usage of the editor_did_unfocus_field hook, fortunately.
+        # Check the following URL for details: https://addon-docs.ankiweb.net/hooks-and-filters.html
+        return changed
     current_field_name = note.note_type()['flds'][field_idx]['name']
     if current_field_name == "Sentence":
         sentence = note.fields[field_idx]
         if not sentence.strip():
             # return when "sentence" is empty (empty spaces are deemed as empty)
-            return
+            return changed
 
         sentence_voice_field = get_field_by_name("Sentence Voice", note)
         sentence_voice_field_contents = note.fields[sentence_voice_field['ord']]
         if sentence_voice_field_contents.strip():
             # We don't update the "Sentence Voice" field since it's already filled
-            return
+            return changed
 
         # Try fetching the voice
         filename = generate_voice(sentence)
         if filename is None:
             # Show an error message
             showInfo("Failed to generate voice. Please try again.")
-            return
+            return changed
         note.fields[sentence_voice_field['ord']] = f"[sound:{filename}]"
-        active_window = aqt.mw.app.activeWindow()
-        if not active_window:
-            return
-        editor = active_window.editor
-        editor.loadNote()
+        return True
 
 
 def generate_voice(sentence: str) -> Optional[str]:
@@ -68,7 +67,7 @@ def generate_voice(sentence: str) -> Optional[str]:
 
     body = f"""
     <speak version='1.0' xml:lang='en-US'><voice xml:lang='en-US' xml:gender='Female' name='en-US-JaneNeural'>
-        {sentence}
+        {sentence.replace('&nbsp;', ' ')}
     </voice></speak>
     """
 
